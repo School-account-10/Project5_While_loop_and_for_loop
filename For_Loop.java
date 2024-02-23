8020af65e892afcaf47f8f8880f3bfa6bc865133
じゅういち <160393316+School-account-10@users.noreply.github.com>
2024-02-23 16:06:48 +0000

    Add files via upload

---
+++ For_Loop.java
@@ -0,0 +1,10 @@
+public class For_Loop {
+    public static void main(String[] args) {
+        for (int counter=5;  counter>=0; counter-- ) {
+            System.out.println(counter);
+
+        }
+    }
+
+    
+}
---
+++ While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+    int counter = 5;
+    while (counter<=0) {
+    System.out.println(counter);
+    counter--;
+            
+        }
+    }
+    
+}
